2e48fc53903d41b51182771d49ff94f04ef06650
Emmanuel B. Agyei <91382543+emmanuel-b-agyei@users.noreply.github.com>
2024-04-05 09:57:47 +0000

    Delete chore_management directory

--- chore_management/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Start a PHP session
-session_start();
-
-header('Location: ./login/login_view.php');
--- chore_management/login/login_view.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'connection.php';
-
-include './view/login_user_action.php';
-
-header('Location: ../view/Login_Page.html', true, 302);
-exit;
-?>
