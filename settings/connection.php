5916d09d13ac5fc49464031c167db6d1b9d1948c
Emmanuel B. Agyei <91382543+emmanuel-b-agyei@users.noreply.github.com>
2024-04-09 10:34:28 +0000

    Update connection.php

--- settings/connection.php
+++ settings/connection.php
@@ -16,12 +16,12 @@ if (!defined('AZURE_MYSQL_DBNAME')) {
     define('AZURE_MYSQL_DBNAME', 'chores_mgt');
 }
 
-if (!defined('DB_PORT')) {
+if (!defined('AZURE_MYSQL_PORT')) {
     define('AZURE_MYSQL_PORT', '3306');
 }
 
 // Establish a database connection using the defined constants
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+$conn = new mysqli(AZURE_MYSQL_HOST, AZURE_MYSQL_USERNAME, AZURE_MYSQL_PASSWORD, AZURE_MYSQL_DBNAME, AZURE_MYSQL_PORT. );
 
 // Checking connection
 if ($conn->connect_error) {
